4365bc7737249c4b5af13e7fb512a1f8a1ef908c
sathappanspm <sathappan.spm@gmail.com>
2015-03-17 17:17:32 -0400

    added exception utils

---
+++ genericUtils/exceptionUtils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""
+    *.py: Description of what * does.
+    Last Modified:
+"""
+
+__author__ = "Sathappan Muthiah"
+__email__ = "sathap1@vt.edu"
+__version__ = "0.0.1"
+
+from functool import wraps
+import signal
+import os
+import errno
+
+
+class TimeOutError(Exception):
+    pass
+
+
+def timeout(seconds=10, errormsg=os.strerror(errno.ETIME)):
+    """
+    Decorator function to cuase timeout after specified time
+
+    """
+    def decorator(func):
+        def _timeoutHandler(errormsg):
+            return TimeOutError(errormsg)
+
+        @wraps
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _timeoutHandler)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+
+            return result
+        return wrapper
+    return decorator
